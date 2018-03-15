62d55f228e4149b924bca7c508fb324dbbc2afdc
Tine Salangsang <tine.salangsang@gmail.com>
2018-03-15 10:27:58 +0800

    Auto-generated commit

--- assets/scripts/master.js
+++ assets/scripts/master.js
@@ -1,6 +1,22 @@
 $(document).ready(function(e) {
+
     $('img[usemap]').rwdImageMaps();
+
     $('area').data('maphilight');
 
-    
+    $('.upload-photo .upload .browse, .upload-photo .upload input').click(function() {
+    	event.preventDefault();
+	    $('.profile-picture')[0].click();
+	});
+
+    $(document).on('change', '.profile-picture', function(event) {
+    	$('.upload input').val($(this)[0].files[0].name);
+
+    	return false;
+    });
+
+    $(document).on('click', '.goNext', function(event) {
+    	event.preventDefault();
+    	$("#tourSlide").slick('slickNext');
+    });
 });
