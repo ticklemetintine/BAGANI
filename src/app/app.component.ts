ab87ee12c4b5d4eb8a0e738869a0f8b58da4a847
Tine Salangsang <tine.salangsang@gmail.com>
2018-03-19 18:52:43 +0800

    [update] wikia region

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
 import {Component} from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+	selector: 'app-root',
+	templateUrl: './app.component.html',
 })
 export class AppComponent {
 
+	constructor(
+		private router: Router,
+
+	) { }
+
+	ngOnInit() {
+		this.router.events.subscribe((evt) => {
+			if (!(evt instanceof NavigationEnd)) {
+				return;
+			}
+			window.scrollTo(0, 0)
+		});
+	}
 }
