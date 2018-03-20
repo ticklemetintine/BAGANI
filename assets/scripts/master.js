b792888793a662e9e2d428cb8dab6589eb7cbab9
Tine Salangsang <tine.salangsang@gmail.com>
2018-03-20 14:36:34 +0800

    Auto-generated commit

--- assets/scripts/master.js
+++ assets/scripts/master.js
@@ -5,18 +5,49 @@ $(document).ready(function(e) {
     $('area').data('maphilight');
 
     $('.upload-photo .upload .browse, .upload-photo .upload input').click(function() {
-    	event.preventDefault();
-	    $('.profile-picture')[0].click();
-	});
+        event.preventDefault();
+        $('.profile-picture')[0].click();
+    });
 
     $(document).on('change', '.profile-picture', function(event) {
-    	$('.upload input').val($(this)[0].files[0].name);
+        $('.upload input').val($(this)[0].files[0].name);
 
-    	return false;
+        return false;
     });
 
     $(document).on('click', '.goNext', function(event) {
-    	event.preventDefault();
-    	$("#tourSlide").slick('slickNext');
+        event.preventDefault();
+        $("#tourSlide").slick('slickNext');
+    });
+
+    $(".alamat .content").on("afterChange", function() {
+        var bg = $('.slick-active .alamat-background').val();
+        $('.alamat').css({
+            'background': 'url('+bg+') top center no-repeat fixed',
+            'background-size': 'cover'
+        });
+    });
+
+    $(document).on('click', 'header .menu-container .burger-menu', function(event) {
+        event.preventDefault();
+        var burgerMenu = $('header .menu-container').clone();
+
+        $('body').addClass('menu-opened');
+        $('.menu-expanded').fadeIn('fast');
+
+        $('.menu-expanded > .container').prepend(burgerMenu);
+    });
+
+    $(document).on('click', '.menu-expanded .menu-container .burger-menu', function(event) {
+        event.preventDefault();
+        $('body').removeClass('menu-opened');
+        $('.menu-expanded').fadeOut('fast');
+
+        $('.menu-expanded > .container .menu-container').remove();
+    });
+
+    $(document).on('click', '.menu-expanded .main-menu .sansinukob a ', function(event) {
+        $('.menu-expanded .main-menu .sansinukob .sub').slideToggle();
+        $('.menu-expanded .main-menu .sansinukob').toggleClass('expanded');
     });
 });
