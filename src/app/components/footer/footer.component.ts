bb43f3fb0486df77cd04160cff82d4e73a527571
Tine Salangsang <tine.salangsang@gmail.com>
2018-03-22 16:36:05 +0800

    terms and privacy modal

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styles: []
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styles: []
 })
 export class FooterComponent implements OnInit {
+	showModalPrivacy:boolean = false;
+	showModalTerms:boolean = false;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+    closeModal() {
+    	this.showModalPrivacy = false;
+    	this.showModalTerms = false;
+    }
+    modalPrivacy() {
+    	this.showModalPrivacy = true;
+    }
+    modalTerms() {
+    	this.showModalTerms = true;
+    }
 
 }
