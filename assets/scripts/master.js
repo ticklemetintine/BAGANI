b54d62df5271a931fd635c3fb9ecb953045d57b4
Tine Salangsang <tine.salangsang@gmail.com>
2018-04-04 14:07:17 +0800

    Auto-generated commit

--- assets/scripts/master.js
+++ assets/scripts/master.js
@@ -1,41 +1,4 @@
 $(document).ready(function(e) {
-    var video = document.querySelector('video'), container = document.querySelector('body');
-
-    var setVideoDimensions = function() {
-        // Video's intrinsic dimensions
-        var w = video.videoWidth,
-            h = video.videoHeight;
-
-        var videoRatio = (w / h).toFixed(2);
-
-        // Get the container's computed styles
-        //
-        // Also calculate the min dimensions required (this will be
-        // the container dimentions)
-        var containerStyles = window.getComputedStyle(container),
-            minW = parseInt(containerStyles.getPropertyValue('width')),
-            minH = parseInt(containerStyles.getPropertyValue('height'));
-
-        var widthRatio = minW / w,
-            heightRatio = minH / h;
-
-        // Whichever ratio is more, the scaling
-        // has to be done over that dimension
-        if (widthRatio > heightRatio) {
-            var newWidth = minW;
-            var newHeight = Math.ceil(newWidth / videoRatio);
-        } else {
-            var newHeight = minH;
-            var newWidth = Math.ceil(newHeight * videoRatio);
-        }
-
-        video.style.width = newWidth + 'px';
-        video.style.height = newHeight + 'px';
-    };
-
-    video.addEventListener('loadedmetadata', setVideoDimensions, false);
-    window.addEventListener('resize', setVideoDimensions, false);
-
     $(document).on("scroll", function() {
         var scroll = $(window).scrollTop();
 
